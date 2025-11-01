6d2dd82e6b9bf7f9b711d93516c98ea3069bf6e8
Sasha Dunaizev <jenderprolite@gmail.com>
2025-11-02 00:02:27 +0500

    Task5

---
+++ Tyuiu.DunaizevAO.Sprint4.Task5.V15.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.DunaizevAO.Sprint4.Task5.V15.Lib
+{
+    public class DataService : ISprint4Task5V15
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int res = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (matrix[i, j] > 0)
+                    {
+                        res += matrix[i, j];
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.DunaizevAO.Sprint4.Task5.V15/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.DunaizevAO.Sprint4.Task5.V15.Lib;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+int[,] array = new int[5, 5];
+for (int i = 0; i < 5; i++)
+{
+	for (int j = 0; j < 5; j++)
+	{
+		array[i, j] = int.Parse(Console.ReadLine());	
+	}
+}
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.Calculate(array));
+
+Console.ReadLine();
